b7ac18508c8eeb6a04499879f8cf45573d579d72
Rusydy <rusydymuhiddin@gmail.com>
2022-10-02 15:00:13 +0700

    UPDATE: split express server with websocket

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // initiate express
-const express = require('express');
-const app = express();
+const express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    websocket = require('ws'),
+    wss = new websocket.Server({ server }),
+    port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,8 +15,14 @@ const routes = require('./routes');
 app.use('/', routes);
 
 // initiate server
-const port = 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
 
+// initiate websocket
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(`Received message => ${message}`);
+    });
+    ws.send('Hello! Message From Server!!');
+});
