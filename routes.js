70764bd7a37f09fee0d6816ed0c81ba47c4062ba
Rusydy <rusydymuhiddin@gmail.com>
2022-10-02 13:15:47 +0700

    change folder name from modules src

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const invoiceController = require("./modules/invoice/invoice.controller");
+const invoiceController = require("./src/invoice/invoice.controller");
 
 router.get("/", (req, res) => {
     res.send("Hello World");
